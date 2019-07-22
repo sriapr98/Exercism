a2873722a77188fb16c78e44611c255eff47194a
Srikkanth S <sriapr98@gmail.com>
2019-07-22 20:13:58 +0530

    difsquarecommit

---
+++ difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,17 @@
+import java.math.*;
+class DifferenceOfSquaresCalculator {
+
+
+    int computeSquareOfSumTo(int input) {
+        return (int)(Math.pow(input*(input+1)/2,2));
+    }
+
+    int computeSumOfSquaresTo(int input) {
+        return input*(input+1)*(2*input+1)/6;
+    }
+
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input)-computeSumOfSquaresTo(input);
+    }
+
+}
